cdf3f05a12ded8e4078cb95aab1fa896d543a7c4
Thibaud Arguillere <targuillere@nuxeo.com>
2014-10-12 19:55:03 -0400

    Add more unit tests

--- nuxeo-imagemetadata-utils-plugin/src/test/java/org/nuxeo/imagemetadata/test/ImageMetadataReaderTest.java
+++ nuxeo-imagemetadata-utils-plugin/src/test/java/org/nuxeo/imagemetadata/test/ImageMetadataReaderTest.java
@@ -22,15 +22,24 @@ import static org.junit.Assert.*;
 import java.io.File;
 import java.util.HashMap;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.nuxeo.common.utils.FileUtils;
+import org.nuxeo.ecm.automation.AutomationService;
+import org.nuxeo.ecm.automation.OperationChain;
+import org.nuxeo.ecm.automation.OperationContext;
+import org.nuxeo.ecm.automation.core.util.Properties;
+import org.nuxeo.ecm.automation.test.EmbeddedAutomationServerFeature;
 import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.impl.blob.FileBlob;
 import org.nuxeo.ecm.core.test.CoreFeature;
 import org.nuxeo.ecm.platform.test.PlatformFeature;
 import org.nuxeo.imagemetadata.ImageMetadataConstants.RESOLUTION_UNITS;
 import org.nuxeo.imagemetadata.ImageMetadataReader;
+import org.nuxeo.imagemetadata.SavePictureMeadataInDocument;
 import org.nuxeo.imagemetadata.ImageMetadataConstants.METADATA_KEYS;
 import org.nuxeo.imagemetadata.XYResolutionDPI;
 import org.nuxeo.runtime.test.runner.Deploy;
@@ -40,8 +49,9 @@ import org.nuxeo.runtime.test.runner.FeaturesRunner;
 import com.google.inject.Inject;
 
 @RunWith(FeaturesRunner.class)
-@Features({ PlatformFeature.class, CoreFeature.class })
-@Deploy({ "nuxeo-imagemetadata-utils" })
+@Features({ PlatformFeature.class, CoreFeature.class,
+        EmbeddedAutomationServerFeature.class })
+@Deploy({ "org.nuxeo.ecm.platform.picture.core", "nuxeo-imagemetadata-utils" })
 public class ImageMetadataReaderTest {
 
     private static final String IMAGE_GIF = "images/a.gif";
@@ -56,50 +66,111 @@ public class ImageMetadataReaderTest {
             METADATA_KEYS.HEIGHT, METADATA_KEYS.COLORSPACE,
             METADATA_KEYS.RESOLUTION, METADATA_KEYS.UNITS };
 
+    protected File filePNG;
+
+    protected File fileGIF;
+
+    protected File fileTIF;
+
+    protected File fileJPEG;
+
+    protected DocumentModel parentOfTestDocs;
+
+    protected DocumentModel docPNG;
+
+    protected DocumentModel docGIF;
+
+    protected DocumentModel docTIF;
+
+    protected DocumentModel docJPEG;
+
     @Inject
     CoreSession coreSession;
+
+    @Inject
+    AutomationService service;
 
     @Before
     public void setUp() {
         // Setup documents if needed, etc.
+        filePNG = FileUtils.getResourceFileFromContext(IMAGE_PNG);
+        fileGIF = FileUtils.getResourceFileFromContext(IMAGE_GIF);
+        fileTIF = FileUtils.getResourceFileFromContext(IMAGE_TIF);
+        fileJPEG = FileUtils.getResourceFileFromContext(IMAGE_JPEG);
+
+        // Cleanup repo if needed and create the Picture documents
+        //coreSession.removeChildren(coreSession.getRootDocument().getRef());
+        parentOfTestDocs = coreSession.createDocumentModel("/",
+                "test-pictures", "Folder");
+        parentOfTestDocs.setPropertyValue("dc:title", "test-pictures");
+        parentOfTestDocs = coreSession.createDocument(parentOfTestDocs);
+        parentOfTestDocs = coreSession.saveDocument(parentOfTestDocs);
+
+        docPNG = createPictureDocument(filePNG);
+        docGIF = createPictureDocument(fileGIF);
+        docTIF = createPictureDocument(fileTIF);
+        docJPEG = createPictureDocument(fileJPEG);
+        coreSession.save();
     }
 
-    private void checkValues(String inWhichOne, String inWidth, String inHeight,
+    @After
+    public void cleanup() {
+        coreSession.removeDocument(parentOfTestDocs.getRef());
+        coreSession.save();
+    }
+
+    protected DocumentModel createPictureDocument(File inFile) {
+
+        DocumentModel pictDoc = coreSession.createDocumentModel(
+                parentOfTestDocs.getPathAsString(), inFile.getName(), "Picture");
+        pictDoc.setPropertyValue("dc:title", inFile.getName());
+        pictDoc.setPropertyValue("file:content", new FileBlob(inFile));
+        return coreSession.createDocument(pictDoc);
+
+    }
+
+    protected void checkValues(File inWhichOne, String inWidth, String inHeight,
             String inColorspace, String inResolution, String inUnits, int xDPI,
             int yDPI) throws Exception {
 
-        File theFile = FileUtils.getResourceFileFromContext(inWhichOne);
-        ImageMetadataReader imdr = new ImageMetadataReader(theFile.getAbsolutePath());
+        String fileName = inWhichOne.getName();
+        ImageMetadataReader imdr = new ImageMetadataReader(
+                inWhichOne.getAbsolutePath());
 
         HashMap<METADATA_KEYS, String> result = imdr.getMetadata(KEYS);
-        assertNotNull(inWhichOne, result);
+        assertNotNull(fileName, result);
 
-        assertEquals(inWhichOne, inWidth, result.get(METADATA_KEYS.WIDTH));
-        assertEquals(inWhichOne, inHeight, result.get(METADATA_KEYS.HEIGHT));
-        assertEquals(inWhichOne, inColorspace, result.get(METADATA_KEYS.COLORSPACE));
-        assertEquals(inWhichOne, inResolution, result.get(METADATA_KEYS.RESOLUTION));
-        assertEquals(inWhichOne, inUnits, result.get(METADATA_KEYS.UNITS));
+        assertEquals(fileName, inWidth, result.get(METADATA_KEYS.WIDTH));
+        assertEquals(fileName, inHeight, result.get(METADATA_KEYS.HEIGHT));
+        assertEquals(fileName, inColorspace,
+                result.get(METADATA_KEYS.COLORSPACE));
+        assertEquals(fileName, inResolution,
+                result.get(METADATA_KEYS.RESOLUTION));
+        assertEquals(fileName, inUnits, result.get(METADATA_KEYS.UNITS));
 
         // Resolution needs extra work
         XYResolutionDPI dpi = new XYResolutionDPI(
                 result.get(METADATA_KEYS.RESOLUTION),
                 result.get(METADATA_KEYS.UNITS));
-        assertEquals(inWhichOne, xDPI, dpi.getX());
-        assertEquals(inWhichOne, yDPI, dpi.getY());
+        assertEquals(fileName, xDPI, dpi.getX());
+        assertEquals(fileName, yDPI, dpi.getY());
     }
 
     @Test
     public void testAllImages() throws Exception {
-        checkValues(IMAGE_PNG, "100", "100", "sRGB", "37.79x37.79", "PixelsPerCentimeter", 96, 96);
-        checkValues(IMAGE_GIF, "328", "331", "sRGB", "72x72", "Undefined", 72, 72);
-        checkValues(IMAGE_TIF, "438", "640", "sRGB", "72x72", "PixelsPerInch", 72, 72);
-        checkValues(IMAGE_JPEG, "1597", "232", "sRGB", "96x96", "PixelsPerInch", 96, 96);
+        checkValues(filePNG, "100", "100", "sRGB", "37.79x37.79",
+                "PixelsPerCentimeter", 96, 96);
+        checkValues(fileGIF, "328", "331", "sRGB", "72x72", "Undefined", 72, 72);
+        checkValues(fileTIF, "438", "640", "sRGB", "72x72", "PixelsPerInch",
+                72, 72);
+        checkValues(fileJPEG, "1597", "232", "sRGB", "96x96", "PixelsPerInch",
+                96, 96);
     }
 
     @Test
     public void testGetAllMetadata() throws Exception {
-        File theFile = FileUtils.getResourceFileFromContext(IMAGE_PNG);
-        ImageMetadataReader imdr = new ImageMetadataReader(theFile.getAbsolutePath());
+        ImageMetadataReader imdr = new ImageMetadataReader(
+                filePNG.getAbsolutePath());
         String all = imdr.getAllMetadata();
         assertTrue(all != null);
         assertTrue(!all.isEmpty());
@@ -110,20 +181,85 @@ public class ImageMetadataReaderTest {
 
     @Test
     public void testXYResolutionDPI() throws Exception {
-        XYResolutionDPI xyDPI = new XYResolutionDPI("180x180", RESOLUTION_UNITS.PIXELS_PER_INCH.toString());
+        XYResolutionDPI xyDPI = new XYResolutionDPI("180x180",
+                RESOLUTION_UNITS.PIXELS_PER_INCH.toString());
         assertEquals(180, xyDPI.getX());
         assertEquals(180, xyDPI.getY());
 
-        xyDPI = new XYResolutionDPI("37.89x37.89", RESOLUTION_UNITS.PIXELS_PER_CENTIMETER.toString());
+        xyDPI = new XYResolutionDPI("37.89x37.89",
+                RESOLUTION_UNITS.PIXELS_PER_CENTIMETER.toString());
         assertEquals(96, xyDPI.getX());
         assertEquals(96, xyDPI.getY());
 
-        xyDPI = new XYResolutionDPI("72x72", RESOLUTION_UNITS.UNDEFINED.toString());
+        xyDPI = new XYResolutionDPI("72x72",
+                RESOLUTION_UNITS.UNDEFINED.toString());
         assertEquals(72, xyDPI.getX());
         assertEquals(72, xyDPI.getY());
 
-        xyDPI = new XYResolutionDPI("", RESOLUTION_UNITS.PIXELS_PER_INCH.toString());
+        xyDPI = new XYResolutionDPI("",
+                RESOLUTION_UNITS.PIXELS_PER_INCH.toString());
         assertEquals(0, xyDPI.getX());
         assertEquals(0, xyDPI.getY());
+    }
+
+
+    @Test
+    public void testSavePictureMetadataInDocument() throws Exception {
+        OperationContext ctx = new OperationContext(coreSession);
+        assertNotNull(ctx);
+
+        // ========================================
+        // TEST WITH DEFAULT VALUES
+        // ========================================
+        String changeToken = docPNG.getChangeToken();
+        ctx.setInput(docPNG);
+        OperationChain chain = new OperationChain("testChain");
+        // let default value for "xpath", "properties" and "save"
+        chain.add(SavePictureMeadataInDocument.ID);
+        service.run(ctx, chain);
+
+        // Check the doc was modified
+        assertNotSame(changeToken, docPNG.getChangeToken());
+
+        // Check value for this PNG
+        assertEquals((long) 100, docPNG.getPropertyValue("imd:pixel_xdimension"));
+        assertEquals((long) 100, docPNG.getPropertyValue("imd:pixel_ydimension"));
+        assertEquals("sRGB", docPNG.getPropertyValue("imd:color_space"));
+        assertEquals((long) 96, docPNG.getPropertyValue("imd:xresolution"));
+        assertEquals((long) 96, docPNG.getPropertyValue("imd:yresolution"));
+
+        // ========================================
+        // ASK FOR ALL PROPERTIES
+        // ========================================
+        changeToken = docPNG.getChangeToken();
+        ctx.setInput(docPNG);
+        chain = new OperationChain("testChain");
+        // Let xpath and save the default values
+        Properties props = new Properties();
+        props.put("dc:description", "all");
+        chain.add(SavePictureMeadataInDocument.ID).set("properties", props);
+        service.run(ctx, chain);
+
+        // Check the doc was modified
+        assertNotSame(changeToken, docPNG.getChangeToken());
+
+        // Check value for this PNG
+        String all = (String) docPNG.getPropertyValue("dc:description");
+        assertTrue(all != null && !all.isEmpty());
+        // Possibly, check some values are available
+        assertTrue(all.indexOf("Page geometry") > -1);
+        assertTrue(all.indexOf("Units") > -1);
+
+        // ========================================
+        // NO SAVE MUST, WELL, NOT SAVE
+        // ========================================
+        changeToken = docPNG.getChangeToken();
+        ctx.setInput(docPNG);
+        chain = new OperationChain("testChain");
+        // Let xpath and properties the default values
+        chain.add(SavePictureMeadataInDocument.ID).set("save", false);
+        service.run(ctx, chain);
+
+        assertEquals(changeToken, docPNG.getChangeToken());
     }
 }
